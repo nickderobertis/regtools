417381bd568d49b8215075682722bbbc777e122f
Nick DeRobertis <whoopnip@gmail.com>
2020-02-17 15:23:33 -0500

    Add remaining imports to __init__

--- regtools/__init__.py
+++ regtools/__init__.py
@@ -3,7 +3,7 @@ High-level tools for running regressions. Handles fixed effects, 2+ way clusteri
 lagged variables, differenced variables, interaction effects, iteration tools, and producing summaries for a
 variety of models including OLS, Logit, Probit, Quantile, and Fama-Macbeth.
 """
-from .iter import (
+from regtools.iter import (
     reg_for_each_combo,
     reg_for_each_xvar_set,
     reg_for_each_combo_select_and_produce_summary,
@@ -13,8 +13,9 @@ from .iter import (
     reg_for_each_lag,
     reg_for_each_lag_and_produce_summary
 )
-from .reg import reg
-from .summarize import produce_summary
-from .select import select_models
-from .args import RegressionSetArgs
-
+from regtools.reg import reg
+from regtools.linmodels.reg import linear_reg
+from regtools.differenced import diff_reg
+from regtools.quantile import quantile_reg
+from regtools.summarize import produce_summary
+from regtools.select import select_models
